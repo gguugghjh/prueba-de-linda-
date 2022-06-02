4e92105d640b9f6c87c557cdacc61a2fb510d6e4
gguugghjh <106721796+gguugghjh@users.noreply.github.com>
2022-06-01 22:59:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('ya ves como si eres linda uwu')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover',function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('trasnform',`trasnlate(-${randomX}%,-${randomY}%)`);
+})
